54bd992ccf7f44524389141cee1bd397c5958c8c
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-04-28 14:51:50 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var masivi =[1,2,3,4,5,6,7,8,9,10];
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+masivi.pop();
+document.write(masivi);
+
+
+
+    
+
+   
+
+
